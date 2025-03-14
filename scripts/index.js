e32cbe17ed90fb8ad8c025cc82867ac59c3be747
Modesto <mofesdo.dev@gmail.com>
2025-03-14 02:23:32 -0700

    Docs: Changed comments in index.js for better understanding

--- scripts/index.js
+++ scripts/index.js
@@ -21,25 +21,25 @@ cards.forEach((card) => {
     const cardItem = document.createElement("li");
     cardItem.classList.add("card");
 
-    //Create the link for the card, add appropriate class and append to card item
+    //Create the link for the card, add appropriate class
     const cardLink = document.createElement("a");
     cardLink.href = card.link;
     cardLink.classList.add("card__link")
 
-    //Create the card image, add appropriate class and append to card item
+    //Create the card image, add appropriate class and append to card link
     const cardImg = document.createElement("img");
     cardImg.classList.add("card__image")
     cardImg.src = card.img;
     cardImg.alt = card.alt;
     cardLink.appendChild(cardImg);
 
-    //Create the card header, add appropriate class and append to card item
+    //Create the card header, add appropriate class and append to card link
     const cardHeader = document.createElement("h2");
     cardHeader.classList.add("card__header")
     cardHeader.textContent = card.header;
     cardLink.appendChild(cardHeader);
 
-    //Finally append the card item to the card list
+    //Finally append the card link to the card item then to the card list
     cardItem.append(cardLink);
     cardList.append(cardItem);
 })
