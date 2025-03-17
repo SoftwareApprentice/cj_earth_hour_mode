a1adc830dd1ac0fc9ecf37cfd9bf68c3f871cb90
Aaron Liang <liangaaron03315@gmail.com>
2025-03-17 17:24:01 -0400

    fix: fa fa bar functionality

--- scripts/index.js
+++ scripts/index.js
@@ -4,39 +4,68 @@ import Card from "../components/Card.js";
 import Pledge from "../components/Pledge.js";
 
 const cardList = new Section(
-    {
-      items: constants.cards,
-      renderer: (item) => new Card(item, constants.cardSelectors, {
+  {
+    items: constants.cards,
+    renderer: (item) =>
+      new Card(item, constants.cardSelectors, {
         popupRenderer: (data) => {
-          const popup = document.querySelector(constants.popupCardInfoSelectors.popup);
+          const popup = document.querySelector(
+            constants.popupCardInfoSelectors.popup
+          );
           popup.classList.add("popup_visible");
-          popup.querySelector(constants.popupCardInfoSelectors.popupTitle).textContent = data.title;
-          popup.querySelector(constants.popupCardInfoSelectors.popupText).textContent = data.text;
-          popup.querySelector(constants.popupCardInfoSelectors.popupCloseButton).addEventListener("click", () => {
-            popup.classList.remove("popup_visible");
-          });
-        }
-      }).getCard()
-    },
-    constants.cardSelectors.cardList
+          popup.querySelector(
+            constants.popupCardInfoSelectors.popupTitle
+          ).textContent = data.title;
+          popup.querySelector(
+            constants.popupCardInfoSelectors.popupText
+          ).textContent = data.text;
+          popup
+            .querySelector(constants.popupCardInfoSelectors.popupCloseButton)
+            .addEventListener("click", () => {
+              popup.classList.remove("popup_visible");
+            });
+        },
+      }).getCard(),
+  },
+  constants.cardSelectors.cardList
 );
 cardList.renderItems();
 
 const pledgeList = new Section(
-    {
-      items: constants.initialPledges,
-      renderer: (item) => new Pledge(item, constants.pledgeSelectors).getPledge()
-    },
-    constants.pledgeSelectors.pledgeList
+  {
+    items: constants.initialPledges,
+    renderer: (item) => new Pledge(item, constants.pledgeSelectors).getPledge(),
+  },
+  constants.pledgeSelectors.pledgeList
 );
 pledgeList.renderItems();
 
-document.querySelector(constants.pledgeSelectors.pledgeForm).addEventListener("submit", () => {
-  pledgeList.addItem(addPledge());
-});
+document
+  .querySelector(constants.pledgeSelectors.pledgeForm)
+  .addEventListener("submit", () => {
+    pledgeList.addItem(addPledge());
+  });
 
 function addPledge() {
-  const name = document.querySelector(constants.pledgeSelectors.pledgeInputName).value;
-  const pledge = document.querySelector(constants.pledgeSelectors.pledgeInputText).value;
+  const name = document.querySelector(
+    constants.pledgeSelectors.pledgeInputName
+  ).value;
+  const pledge = document.querySelector(
+    constants.pledgeSelectors.pledgeInputText
+  ).value;
   return { name, pledge };
 }
+
+//nav bar elements
+const navBar = document.querySelector("#nav-bar"); //.nav_opened
+const navLogo = navBar.querySelector(".nav__logo"); //.nav__logo_hidden
+const navList = navBar.querySelector(".nav__list"); //.nav__list_opened  .nav__list_opened li:not(:last-child)
+
+function myFunction() {
+  navBar.classList.toggle("nav_opened");
+  navLogo.classList.toggle("nav__logo_hidden");
+  navList.classList.toggle("nav__list_opened");
+}
+
+const navIcon = document.querySelector(".nav__icon");
+navIcon.addEventListener("click", myFunction);
